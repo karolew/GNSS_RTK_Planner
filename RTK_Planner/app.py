3da2c706edac8e973e0158ca181c96f63620bb98
karolew <fratczak.karolew@gmail.com>
2025-03-13 23:35:18 +0100

    Cleanup. Part of popup with new rover.

--- RTK_Planner/app.py
+++ RTK_Planner/app.py
@@ -1,35 +1,13 @@
-from flask import render_template, request, jsonify
+from flask import render_template
 import config
-import json
-import rover
-import datetime
 
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yaml")
 
 
-# @app.route('/rover')
-# def get_rovers():
-#     rovers = rover.Rover.query.all()
-#     return jsonify(rover.rovers_schema.dump(rovers))
-
-
-# @app.route('/trail/<int:rover_id>')
-# def get_trails_by_rover(rover_id):
-#     this_rover = rover.Rover.query.get_or_404(rover_id)
-#     rover_trails = this_rover.trails
-#     return jsonify(rover.rover_with_trails_schema.dump(rover_trails))   # ? rover_with_trails_schema
-
-
 @app.route("/")
 def home():
-    #all_rovers = rover.get_all()
-    return render_template("index.html")#, rovers=all_rovers)
-
-
-# @app.route("/get_coords")
-# def get_coords():
-#     return jsonify(rover.latest_gps_data)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
