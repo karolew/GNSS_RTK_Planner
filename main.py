9c838181c3416b6e17ff56de33e2552f523c60b7
karolew <fratczak.karolew@gmail.com>
2025-05-18 21:32:11 +0200

    Refactor main. Update compass submodule.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ except:
     import binascii as ubin
 
 
-def main():
+if __name__ == "__main__":
     # Load configuration.
     with open("config.json") as cfgf:
         config = json.load(cfgf)
@@ -54,10 +54,13 @@ def main():
     # Rover must be registered in RTK planner first.
     rtk_planner.register()
 
-    # Main loop.
-    check_updates_s = time.time()   # interval for trail pull.
+    # Main loop variables.
+    check_updates_s = time.time()   # Timer for trail pull.
+    check_updates_interval_s = 5    # Interval for trail pull.
     previous_coordinates = (0, 0)   # To track coordinate changes.
+    target_threshold = 50           # TODO should be configurable from portal.
 
+    # Start main loop.
     while True:
         try:
             # Check WLAN status.
@@ -82,60 +85,68 @@ def main():
                     previous_coordinates = micro_nmea.lat, micro_nmea.lon
             
             # HTTP pull trails every 5 seconds.
-            if time.time() - check_updates_s > 5:
+            if time.time() - check_updates_s > check_updates_interval_s:
                 rtk_planner.get_trails()
                 check_updates_s = time.time()
 
             # Navigation part.
+            # If motors are present
             if nav.motors:
-                nav.motors.update()     # Must be here to update stop
-                if rtk_planner.trail_points and nav.compass:
-                    compass_heading = nav.compass.get_heading()
-                    if compass_heading:
-                        nav_status = nav.navigate_to_target((micro_nmea.lon, micro_nmea.lat),
-                                                            rtk_planner.trail_points[0],
-                                                            compass_heading)
-                        if nav_status[3] <= 50:
-                            # If we are on the target point +-5cm, mark as ready.
-                            # Remove current coordinates from the trail.
-                            print("POINT OK ---------------------------------------------", rtk_planner.trail_points[0])
-                            rtk_planner.trail_points.pop(0)
-                            nav.motors.move_stop()
-                        else:
-                            current_heading = compass_heading % 360
-                            diff = abs(current_heading - nav_status[2])
-                            clockwise_diff = (nav_status[2] - current_heading) % 360
-                            counter_clockwise_diff = (current_heading - nav_status[2]) % 360
+                # Must be here to update stop.
+                nav.motors.update()
 
-                            if diff <= nav.direction_threshold or (360 - diff) <= nav.direction_threshold:
-                                nav.motors.forward()
-                            elif clockwise_diff < counter_clockwise_diff:
-                                turn_level = abs(clockwise_diff - nav.direction_threshold)
-                                if turn_level <= 10:
-                                    nav.motors.turn_right(0)
-                                elif 10 < turn_level <= 25:
-                                    nav.motors.turn_right(1)
-                                elif 25 < turn_level <= 45:
-                                    nav.motors.turn_right(2)
-                                else:
-                                    nav.motors.turn_right(3)
-                            elif clockwise_diff >= counter_clockwise_diff:
-                                turn_level = abs(counter_clockwise_diff - nav.direction_threshold)
-                                if turn_level <= 10:
-                                    nav.motors.turn_left(0)
-                                elif 10 < turn_level <= 25:
-                                    nav.motors.turn_left(1)
-                                elif 25 < turn_level <= 45:
-                                    nav.motors.turn_left(2)
-                                else:
-                                    nav.motors.turn_left(3)
-                            else:
-                                print("Stop", *nav_status, compass_heading)
-                                nav.motors.move_stop()
+                # Check trail points are present and compass in working.
+                # Stop motor if these conditions are not meet.
+                if not rtk_planner.trail_points or not nav.compass:
+                    print("Stop")
+                    nav.motors.stop()
+                    continue
+
+                # If compass heading calculation is incorrect or crashed skip it.
+                compass_heading = nav.compass.get_heading()
+                if not compass_heading:
+                    continue
+
+                nav_status = nav.navigate_to_target((micro_nmea.lon, micro_nmea.lat),
+                                                    rtk_planner.trail_points[0],
+                                                    compass_heading)
+                if nav_status[3] <= target_threshold:
+                    # If rover is on the target point +-target_threshold, mark as ready.
+                    # Remove current coordinates from the trail.
+                    nav.motors.move_stop()
+                    print(f"TRAIL POIT REACHED: {rtk_planner.trail_points[0]}")
+                    rtk_planner.trail_points.pop(0)
+                else:
+                    current_heading = compass_heading % 360
+                    diff = abs(current_heading - nav_status[2])
+                    clockwise_diff = (nav_status[2] - current_heading) % 360
+                    counter_clockwise_diff = (current_heading - nav_status[2]) % 360
+
+                    if diff <= nav.direction_threshold or (360 - diff) <= nav.direction_threshold:
+                        nav.motors.forward()
+                    elif clockwise_diff < counter_clockwise_diff:
+                        turn_level = abs(clockwise_diff - nav.direction_threshold)
+                        if turn_level <= 10:
+                            nav.motors.turn_right(0)
+                        elif 10 < turn_level <= 20:
+                            nav.motors.turn_right(1)
+                        elif 20 < turn_level <= 30:
+                            nav.motors.turn_right(2)
+                        else:
+                            nav.motors.turn_right(3)
+                    elif clockwise_diff >= counter_clockwise_diff:
+                        turn_level = abs(counter_clockwise_diff - nav.direction_threshold)
+                        if turn_level <= 10:
+                            nav.motors.turn_left(0)
+                        elif 10 < turn_level <= 20:
+                            nav.motors.turn_left(1)
+                        elif 20 < turn_level <= 30:
+                            nav.motors.turn_left(2)
+                        else:
+                            nav.motors.turn_left(3)
+                    else:
+                        print("Stop", *nav_status, compass_heading)
+                        nav.motors.move_stop()
 
         except Exception as e:
             print(f"Main loop error: {e}")
-
-
-if __name__ == "__main__":
-    main()
